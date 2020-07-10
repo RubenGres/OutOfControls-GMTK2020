a99a62a6641c5a0b12ff38ed91deb52192d55717
Ruben <ruben.g.gres@gmail.com>
2020-07-11 00:10:21 +0200

    first commit

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private RigidBody2D rb;
+    private Boolean isGrounded;
+    public float speed;
+    public float jumpForce;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetKey("up"))
+            {
+                rb.AddForce(Vector3.up * jumpForce);
+                isGrounded = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        isGrounded = true;
+    }
+}
