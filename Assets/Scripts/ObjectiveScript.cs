5b0007d0c48aa8eb8c25a92552d5ed06554da7d4
Matthieu Deloffre <matthieu.techno@gmail.com>
2020-07-12 19:51:29 +0200

    added sounds

--- Assets/Scripts/ObjectiveScript.cs
+++ Assets/Scripts/ObjectiveScript.cs
@@ -6,10 +6,12 @@ public class ObjectiveScript : MonoBehaviour
 {
     public Animator animator;
     public float transitionDelayTime = 1.0f;
-
+    public AudioSource audio;
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        audio.Play();
+        DontDestroyOnLoad(audio);
         StartCoroutine(DelayLoadLevel());
     }
 
