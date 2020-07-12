03e87eecc6fc31aacb92c00a173768d8c20c4191
Ruben <ruben.g.gres@gmail.com>
2020-07-12 20:32:31 +0200

    30mn left

---
+++ Assets/Scripts/ZoomInOut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomInOut : MonoBehaviour
+{
+
+    private float speed = 1f;
+    private float amplitude = 0.01f;
+
+    private Vector3 scaleStart;
+    private void Start()
+    {
+        scaleStart = transform.transform.localScale;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 newScale = scaleStart;
+        float s = Mathf.Sin(Time.frameCount * Time.deltaTime * speed) * amplitude;
+        newScale += new Vector3(s, s, s);
+        transform.localScale = newScale;
+    }
+}
